00c1086a9d47f861d943c5bc1e997e9ecc8d15be
Savannah <savmaxwell@gmail.com>
2022-03-11 14:28:31 -0700

    wip

--- api/actions/actions-middleware.js
+++ api/actions/actions-middleware.js
@@ -1,1 +1,33 @@
-// add middlewares here related to actions
+const Action = require('./actions-model');
+const { actionSchema } = require('../schemas/index');
+
+function validateActionId (req, res, next) {
+    Action.get(req.params.id)
+        .then(action => {
+            if (!action) {
+                res.status(404).json({ message: `Could not find action with id ${req.params.id}`})
+            } else {
+                req.action = action;
+                next()
+            }
+        })
+        .catch(next)
+}
+
+function validateAction (req, res, next) {
+    actionSchema.validate(req.body)
+        .then(validated => {
+            if (!validated) {
+                res.status(400).json({ message: "Must provide notes, description and completion project_id"})
+            } else {
+                req.body = validated;
+                next()
+            }
+        })
+        .catch(next)
+}
+
+module.exports = {
+    validateActionId,
+    validateAction
+}
